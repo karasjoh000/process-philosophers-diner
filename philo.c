460000968de337f95c49d9306798da8a7e0e63a4
John I Karasev <john.karasev@wsu.edu>
2018-03-19 16:49:00 -0700

    add shared metadata

--- philo.c
+++ philo.c
@@ -21,6 +21,7 @@
 #include <stdio.h>
 #include <errno.h>
 #include <string.h>
+#include <stdbool.h>
 
 // for eating times
 #define STDDEV_EAT 3
@@ -32,6 +33,7 @@
 
 
 
+static int semid;
 static int shmid;
 
 // This is needed for the semctl operation.
@@ -43,7 +45,53 @@ union semun {
 				(Linux-specific) */
 };
 
+// keeps track of philosphers info
+typedef struct metaPhil {
+	int thinktime;
+	int eattime;
+	int cycles;
+	bool isAlive;
+	pid_t id;
+} pmeta;
 
+//prints results from the philosphers diner at the end
+void printMeta( pmeta *meta ) {
+	printf("\n\n");
+	for ( int i = 0; i < 5; i++ )
+		printf("philospher %d thinktime=%d eattime=%d cycles=%d pid=%d\n",
+			i,  meta[i].thinktime, meta[i].eattime, meta[i].cycles, meta[i].id);
+}
+
+// This functions just returns ptr to memory that is
+// associated with shmid global var.
+pmeta *attach() {
+
+	void* ptr;
+
+	if ( ( ptr = shmat( shmid, NULL, 0 )) < (void*)0) {
+		fprintf( stderr, "Error on shmat: %s\n", strerror( errno ) );
+		exit(0);
+	}
+
+	return ( pmeta * )ptr;
+}
+
+// creates and returns ptr to shared memory of size size.
+void *getshmptr( int size ) {
+	void *ptr;
+	if ( ( shmid = shmget( IPC_PRIVATE, size, ( SHM_R | SHM_W ) ) ) < 0 ) {
+		fprintf( stderr, "Error on shmget: %s\n", strerror( errno ) );
+		exit(1);
+	}
+
+	ptr = attach();
+
+	if ( shmctl(shmid, IPC_RMID, ( struct shmid_ds * ) NULL ) < 0 ) {
+		fprintf( stderr, "Error on shmctl: %s\n", strerror( errno ) );
+		exit(0);
+	}
+	return ptr;
+}
 
 int randomGaussian( int mean, int stddev ) {
 	double mu = 0.5 + ( double ) mean;
@@ -56,63 +104,66 @@ int randomGaussian( int mean, int stddev ) {
 		return ( int ) floor( mu + sigma * sin( f2 ) * f1 );
 }
 
-void lifeOfPi( int id ) {
+// subroutine where each child lives in.
+void lifeOfPi( int id, pmeta *meta ) {
+
 	struct sembuf sops[2];
 	//assign the correct chopsticks to each philospher.
 	sops[0].sem_num = ( id + 4 ) % 5;
 	sops[1].sem_num = id;
 	sops[0].sem_flg = sops[1].sem_flg = 0;
-	int totEatTime;
+
 	//loop until philospher ate for 100 seconds.
-	for ( totEatTime = 0; totEatTime < 100; ) {
+	for ( meta->eattime = meta->thinktime = meta->cycles =  0; meta->eattime < 100; meta->cycles++ ) {
 		int eatTime;
 		int thinkTime;
 		//request two chopsticks;
 		sops[0].sem_op = sops[1].sem_op = -1;
 
 		// wait for the two chopsticks.
-		if ( semop( shmid, sops, 2 ) < 0 ) {
+		if ( semop( semid, sops, 2 ) < 0 ) {
 			fprintf( stderr, "Error on semop philID->%d : %s\n", id, strerror( errno ) );
 			exit( 1 );
 		}
 
 		// get randomGaussian number for eating.
 		eatTime = abs( randomGaussian(  MEAN_EAT, STDDEV_EAT ) );
-		printf( "philID: %d eattime: %d, toteat:%d\n", id, eatTime, totEatTime );
+		printf( "philID: %d eattime: %d, (total eat:%d)\n", id, eatTime, meta->eattime );
 		sleep( eatTime );
-		totEatTime += eatTime;
+		meta->eattime += eatTime;
 
 		// set both chopsticks to one representing release of
 		// chopsticks.
 		sops[0].sem_op = sops[1].sem_op = 1;
 
 		//put chopsticks back on the table.
-		if ( semop( shmid, sops, 2 ) < 0 ) {
+		if ( semop( semid, sops, 2 ) < 0 ) {
 			fprintf( stderr, "Error on semop philID->%d : %s\n", id, strerror( errno ) );
 			exit( 1 );
 		}
 
 		// After philospher is satisfied, he thinks for a random time.
 		thinkTime = abs( randomGaussian( MEAN_THNK, STDDEV_THNK ) );
-		printf( "philID: %d thinktime: %d\n", id, thinkTime );
+		printf( "philID: %d thinktime: %d (total think: %d)\n", id, thinkTime, meta->thinktime );
+		meta->thinktime += thinkTime;
 		sleep( thinkTime );
 
 	}
 	// after philosher has eat for 100 seconds, he gets killed.
-	printf( "philospher %d is dead\n", id );
+	printf( "philospher %d is dead (process %d)\n", id, meta->id );
 
 	exit( 0 );
 }
 
 int main() {
 
+	// needed for a semctl operation
 	union semun r;
 	r.val = 1;
 
 	//get 5 semaphores representing the chopsticks.
-	shmid = semget( IPC_PRIVATE, 5, S_IRUSR | S_IWUSR );
-
-	if( shmid < 0 ) {
+	semid = semget( IPC_PRIVATE, 5, S_IRUSR | S_IWUSR );
+	if( semid < 0 ) {
 		fprintf( stderr, "Error on semget: %s\n", strerror( errno ) );
 		exit( 1 );
 	}
@@ -121,27 +172,40 @@ int main() {
 	// intially set all flags to 1 representing that
 	// all chopsticks are available.
 	for ( ; philID < 5; philID++ ) {
-		if( semctl( shmid, philID, SETVAL, r ) < 0 ) {
+		if( semctl( semid, philID, SETVAL, r ) < 0 ) {
+			fprintf( stderr, "Error on semctl: %s\n", strerror( errno ) );
+			exit( 1 );
+		}
+		if( semctl( semid, philID, SETVAL, r ) < 0 ) {
 			fprintf( stderr, "Error on semctl: %s\n", strerror( errno ) );
 			exit( 1 );
 		}
 	}
 
+	// borrow shared memory to keep track of philospher metadata.
+	pmeta *sm = ( pmeta * ) getshmptr( sizeof(pmeta) * 5 );
+
 	// create a process for each philospher seeding a different number for
 	// each one.
 	for( philID = 0 ; philID < 5; philID++ ) {
-		srand( philID );
-		if ( ! fork() ) lifeOfPi( philID );
+		pid_t pid;
+		srand( philID + 1 );
+		if ( !( pid = fork() ) ) lifeOfPi( philID, &(sm[philID]) );
+		sm[philID].id = pid; // set pid for each child in shared mem.
+
 	}
 
 	// Wait until all philosphers are killed.
 	for( philID = 0 ; philID < 5; philID++ )
 		wait( NULL );
 
+	// print the results from philosphers diner
+	printMeta(sm);
+
 	printf( "\n\nAll philosphers were successfully assassinated\n\n" );
 
 	//release memory.
-	if( semctl( shmid, -1, IPC_RMID ) < 0 ) {
+	if( semctl( semid, -1, IPC_RMID ) < 0 ) {
 		fprintf( stderr, "Error on semctl: %s\n", strerror( errno ) );
 		exit( 1 );
 	}
